b4ed99f71afc9644ad826ede30d56560831ba3a7
danielkermode <danielkermode@hotmail.co.nz>
2016-06-28 11:58:03 +1200

    scaffold

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
 
-var endpoint = 'https://api.wheretheiss.at/v1/satellites'
+var endpoint = 'http://localhost:3000/pens'
 
-xhr.get(endpoint, function (err, data) {
-  if (err) {
-    console.error(err)
-  }
+document.getElementById("getData").addEventListener('click', function(){
+  xhr.get(endpoint, function (err, data) {
+    if (err) {
+      console.error(err)
+    }
 
-  // In case you're curious
-  console.log(data.body) // FYI: data.body is a string
+    // In case you're curious
+    console.log(data.body) // FYI: data.body is a string
 
-  // Replace 'Space' below with the response
-  var target = document.getElementsByTagName('main')[0]
-  target.innerHTML = greeting({name: 'Space'})
+    // Replace 'Space' below with the response
+    var target = document.getElementsByTagName('main')[0]
+    target.innerHTML = greeting({
+      name: 'Dan',
+      pens: data.body
+    })
+  })
 })
+
+

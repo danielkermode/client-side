061aa43d697aa2ad5b82a74e15689c09424655dc
danielkermode <danielkermode@hotmail.co.nz>
2016-06-28 15:30:37 +1200

    Updates

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 var xhr = require('xhr')
 var greeting = require('./views/greeting.hbs')
-//console.log(xhr)
 var endpoint = 'https://localhost:3000/pens'
 
-// document.getElementById("getData").addEventListener('click', function(){
-  xhr.get(endpoint, function (err, data) {
-    if (err) {
-      console.error(err)
-    }
-    // In case you're curious
-    console.log(data.body) // FYI: data.body is a string
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("getData").addEventListener('click', function(){
+    xhr.get(endpoint, function (err, data) {
+      if (err) {
+        console.error(err)
+      }
+      // In case you're curious
+      console.log(data.body) // FYI: data.body is a string
 
-    // Replace 'Space' below with the response
-    var target = document.getElementsByTagName('main')[0]
-    target.innerHTML = greeting({
-      name: 'Dan',
-      pens: JSON.parse(data.body)
+      // Replace 'Space' below with the response
+      var target = document.getElementsByTagName('main')[0]
+      target.innerHTML = greeting({
+        name: 'Dan',
+        pens: JSON.parse(data.body)
+      })
     })
   })
-// })
+});
+
